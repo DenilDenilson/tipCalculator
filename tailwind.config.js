232804e8fe3a7263c3fd4ed3c50d7c8fbea1a990
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-22 20:28:49 -0500

    tailwind configurado

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.{html, js}"
+  ],
+  theme: {
+    screen: {
+      'xl': '1440px',
+    },
+    
+    extend: {
+      colors: {
+        //### Primary
+
+        'Strong_cyan': 'hsl(172, 67%, 45%)',
+
+        //### Neutral
+
+        'Very_dark_cyan': 'hsl(183, 100%, 15%)',
+        'Dark_grayish_cyan': 'hsl(186, 14%, 43%)',
+        'Grayish_cyan': 'hsl(184, 14%, 56%)',
+        'Light_grayish_cyan': 'hsl(185, 41%, 84%)',
+        'Very_light_grayish_cyan': 'hsl(189, 41%, 97%)',
+        'White': 'hsl(0, 0%, 100%)',
+
+      },
+
+      fontFamily: {
+        'sans': ['Space Mono', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
